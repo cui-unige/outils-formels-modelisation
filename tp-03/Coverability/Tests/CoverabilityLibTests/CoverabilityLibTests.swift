7c1f33a290774d4e17f96ea7479ef838eddad5fa
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-11-24 17:14:10 +0100

    Add a test to tp-03

--- tp-03/Coverability/Tests/CoverabilityLibTests/CoverabilityLibTests.swift
+++ tp-03/Coverability/Tests/CoverabilityLibTests/CoverabilityLibTests.swift
@@ -7,6 +7,7 @@ class CoverabilityLibTests: XCTestCase {
     static let allTests = [
         ("testBoundedGraph"  , testBoundedGraph  ),
         ("testUnboundedGraph", testUnboundedGraph),
+        ("testMultiPaths"    , testMultiPaths),
     ]
 
     func testBoundedGraph() {
@@ -47,6 +48,32 @@ class CoverabilityLibTests: XCTestCase {
             [s0: 1, s1: 0, s2: 1, s3: 0, s4: 1, b: 0]
         let coverabilityGraph = model.coverabilityGraph(from: initialMarking)
         XCTAssertEqual(coverabilityGraph.count, 5)
+    }
+
+    func testMultiPaths() {
+        let p0 = PTPlace(named: "p0")
+        let p1 = PTPlace(named: "p1")
+
+        let model = PTNet(
+            places: [p0, p1],
+            transitions: [
+                PTTransition(
+                    named         : "t0",
+                    preconditions : [PTArc(place: p0, tokens: 2)],
+                    postconditions: [PTArc(place: p1)]),
+                PTTransition(
+                    named         : "t1",
+                    preconditions : [PTArc(place: p1, tokens: 6)],
+                    postconditions: [PTArc(place: p1, tokens: 6), PTArc(place: p0)]),
+                PTTransition(
+                    named         : "t2",
+                    preconditions : [PTArc(place: p0, tokens: 2)],
+                    postconditions: [PTArc(place: p0, tokens: 1), PTArc(place: p1, tokens: 4)])])
+
+        let initialMarking: CoverabilityMarking =
+            [p0: 3, p1: 2]
+        let coverabilityGraph = model.coverabilityGraph(from: initialMarking)
+        XCTAssertEqual(coverabilityGraph.count, 7)
     }
 
 }
